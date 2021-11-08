66e9063a43c2d08c8474b3bf0ee0a447c160eaae
085284051385 <ggridho074@gmail.com>
2021-11-08 17:31:36 +0700

    ocho

---
+++ message/help.js
@@ -0,0 +1,266 @@
+exports.islamiMenu = (prefix) => {
+    return `╭─❒ 「 *ISLAMI MENU* 」
+│ ⬡ ${prefix}alquran
+│ ⬡ ${prefix}asmaulhusna
+│ ⬡ ${prefix}alquranaudio
+│ ⬡ ${prefix}jadwalsholat
+│ ⬡ ${prefix}kisahnabi
+│ ⬡ ${prefix}listsurah
+└──── *[ OCHOBOTZ  ]* ──────❏
+`
+}
+exports.tebakMenu = (prefix) => {
+    return `╭─❒ 「 *TEBAK MENU* 」
+│ ⬡ ${prefix}tebakumur
+│ ⬡ ${prefix}artinama *nama*
+│ ⬡ ${prefix}jodoh
+│ ⬡ ${prefix}jadian
+└──── *[ OCHOBOTZ  ]* ──────❏
+`
+}
+exports.scriptMenu = (prefix) => {
+    return `╭─❒ 「 *TOP UP GAME* 」
+│ ⬡ ${prefix}FREE FIRE
+│ ⬡ ${prefix}MOBILE LEGENDS
+│ ⬡ ${prefix}PUBG
+└❏`
+}
+exports.infoMenu = (prefix) => {
+    return `╭─❒ 「 *INFO MENU* 」
+│ ⬡ ${prefix}kbbi
+│ ⬡ ${prefix}infogempa
+│ ⬡ ${prefix}covidindo
+│ ⬡ ${prefix}covidglobal
+│ ⬡ ${prefix}jadwaltv
+│ ⬡ ${prefix}translate
+│ ⬡ ${prefix}cuaca
+│ ⬡ ${prefix}wikipedia *teks*
+│ ⬡ ${prefix}jarak
+│ ⬡ ${prefix}speed
+│ ⬡ ${prefix}grupbot
+│ ⬡ ${prefix}youtube
+│ ⬡ ${prefix}infoig
+└──── *[ OCHOBOTZ  ]* ──────❏
+`
+}
+exports.stalkMenu = (prefix) => {
+    return `╭─❒ 「 *STALK MENU* 」
+│ ⬡ ${prefix}stalkig *nama*
+│ ⬡ ${prefix}stalkgithub *nama*
+│ ⬡ ${prefix}stalktiktok *nama*
+└──── *[ OCHOBOTZ  ]* ──────❏
+`
+}
+exports.imageMenu = (prefix) => {
+    return `╭─❒ 「 *IMAGE MENU* 」
+│ ⬡ ${prefix}art
+│ ⬡ ${prefix}loli
+│ ⬡ ${prefix}neko
+│ ⬡ ${prefix}waifu
+│ ⬡ ${prefix}husbu
+│ ⬡ ${prefix}wallnime
+└──── *[ OCHOBOTZ ]* ──────❏
+`
+}
+exports.stickerMenu = (prefix) => {
+    return `╭─❒ 「 *STICKER MENU* 」
+│ ⬡ ${prefix}attp *teks*
+│ ⬡ ${prefix}sticker *reply image*
+│ ⬡ ${prefix}tomp3
+│ ⬡ ${prefix}tovideo
+└──── *[ OCHOBOTZ  ]* ──────❏
+`
+}
+exports.searchMenu = (prefix) => {
+    return `╭─❒ 「 *SEARCH MENU* 」
+│ ⬡ ${prefix}gimage
+│ ⬡ ${prefix}wallpapersearch
+│ ⬡ ${prefix}playstore
+│ ⬡ ${prefix}shopee *text*
+│ ⬡ ${prefix}google *text*
+└──── *[ OCHOBOTZ  ]* ──────❏
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `╭─❒ 「 *OWNER MENU* 」
+│ ⬡ ${prefix}owner
+│ ⬡ ${prefix}sewabot
+│ ⬡ ${prefix}bc *teks*
+│ ⬡ ${prefix}report
+└──── *[ OCHOBOTZ L ]* ──────❏
+`
+}
+exports.bugMenu = (prefix) => {
+    return `╭─❒ 「 *BANTAI MENU* 」
+│ ⬡ ${prefix}bugpc2
+│ ⬡ ${prefix}bug *jumlah*
+│ ⬡ ${prefix}bughole
+│ ⬡ ${prefix}bugtroli
+│ ⬡ ${prefix}bugtroli2
+│ ⬡ ${prefix}bugcombine
+│ ⬡ ${prefix}buglokasi
+└──── *[ OCHOBOTZ  ]* ──────❏
+`
+}
+exports.textMenu = (prefix) => {
+    return `╭─❒ 「 *TEXT MENU* 」
+│ ⬡ ${prefix}quotes
+│ ⬡ ${prefix}quotesanime
+│ ⬡ ${prefix}quotesdilan
+│ ⬡ ${prefix}quotesimage
+│ ⬡ ${prefix}katabijak
+│ ⬡ ${prefix}random nama
+└──── *[ OCHOBOTZ  ]* ──────❏
+`
+}
+exports.groupMenu = (prefix) => {
+    return`╭─❒ 「 *MENU GROUP* 」 
+│ ⬡ ${prefix}antilink *on/off*
+│ ⬡ ${prefix}welcome *on/off*
+│ ⬡ ${prefix}antivirtex *on/off*
+│ ⬡ ${prefix}grup *buka/tutup*
+│ ⬡ ${prefix}linkgrup
+│ ⬡ ${prefix}promote *@tag*
+│ ⬡ ${prefix}demote *@tag*
+│ ⬡ ${prefix}add *@tag*
+│ ⬡ ${prefix}kick *@tag*
+│ ⬡ ${prefix}setpp *reply image*
+│ ⬡ ${prefix}setdesc *teks*
+│ ⬡ ${prefix}setname *teks*
+│ ⬡ ${prefix}hidetag *teks*
+│ ⬡ ${prefix}infogc
+└──── *[ OCHOBOTZ  ]* ──────❏
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `╭─❒ 「 *DOWNLOADER* 」
+│ ⬡ ${prefix}play
+│ ⬡ ${prefix}ytsearch *teks*
+│ ⬡ ${prefix}ytmp3 *link*
+│ ⬡ ${prefix}ytmp4 *link*
+│ ⬡ ${prefix}tiktoknowm *link*
+│ ⬡ ${prefix}tiktokmusic
+│ ⬡ ${prefix}pinterest *teks*
+└──── *[ OCHOBOTZ  ]* ──────❏`
+}
+exports.allMenu = (prefix, cmhit, runtime, pushname, isOwner, isPremium, gcount, sender, baterai) => {
+    return `╭─❒ 「 MENU GROUP 」 
+│ ⬡ ${prefix}antilink *on/off*
+│ ⬡ ${prefix}welcome *on/off*
+│ ⬡ ${prefix}antivirtex *on/off*
+│ ⬡ ${prefix}group
+│ ⬡ ${prefix}linkgrup
+│ ⬡ ${prefix}promote *@tag*
+│ ⬡ ${prefix}demote *@tag*
+│ ⬡ ${prefix}add *@tag*
+│ ⬡ ${prefix}kick *@tag*
+│ ⬡ ${prefix}setpp *reply image*
+│ ⬡ ${prefix}setdesc *teks*
+│ ⬡ ${prefix}setname *teks*
+│ ⬡ ${prefix}hidetag *teks*
+└──── *[ OCHOBOTZ  ]* ──────❏
+
+╭─❒ 「 STICKER MENU 」
+│ ⬡ ${prefix}attp *teks*
+│ ⬡ ${prefix}sticker *reply image*
+│ ⬡ ${prefix}tomp3
+│ ⬡ ${prefix}tovideo
+└──── *[ OCHOBOTZ  ]* ──────❏
+	
+╭─❒ 「 DOWNLOADER 」
+│ ⬡ ${prefix}play
+│ ⬡ ${prefix}ytsearch *teks*
+│ ⬡ ${prefix}ytmp3 *link*
+│ ⬡ ${prefix}ytmp4 *link*
+│ ⬡ ${prefix}tiktoknowm *link*
+│ ⬡ ${prefix}tiktokmusic
+│ ⬡ ${prefix}pinterest *teks*
+└──── *[ OCHOBOTZ  ]* ──────❏
+	
+╭─❒ 「 ISLAMI MENU 」
+│ ⬡ ${prefix}alquran
+│ ⬡ ${prefix}asmaulhusna
+│ ⬡ ${prefix}alquranaudio
+│ ⬡ ${prefix}jadwalsholat
+│ ⬡ ${prefix}kisahnabi
+│ ⬡ ${prefix}listsurah
+└──── *[ OCHOBOTZ  ]* ──────❏
+	
+╭─❒ 「 STICKER MENU 」
+│ ⬡ ${prefix}attp *teks*
+│ ⬡ ${prefix}sticker *reply image*
+│ ⬡ ${prefix}tomp3
+│ ⬡ ${prefix}tovideo
+└──── *[ OCHOBOTZ  ]* ──────❏
+	
+╭─❒ 「 INFO MENU 」
+│ ⬡ ${prefix}kbbi
+│ ⬡ ${prefix}infogempa
+│ ⬡ ${prefix}covidindo
+│ ⬡ ${prefix}covidglobal
+│ ⬡ ${prefix}jadwaltv
+│ ⬡ ${prefix}translate
+│ ⬡ ${prefix}cuaca
+│ ⬡ ${prefix}wikipedia *teks*
+│ ⬡ ${prefix}jarak
+└──── *[ OCHOBOTZ  ]* ──────❏
+	
+╭─❒ 「 TEXT MENU 」
+│ ⬡ ${prefix}quotes
+│ ⬡ ${prefix}quotesanime
+│ ⬡ ${prefix}quotesdilan
+│ ⬡ ${prefix}quotesimage
+│ ⬡ ${prefix}katabijak
+│ ⬡ ${prefix}random nama
+└──── *[ OCHOBOTZ  ]* ──────❏
+	
+╭─❒ 「 SEARCH MENU 」
+│ ⬡ ${prefix}gimage
+│ ⬡ ${prefix}wallpapersearch
+│ ⬡ ${prefix}playstore
+│ ⬡ ${prefix}shopee *text*
+│ ⬡ ${prefix}google *text*
+└──── *[ OCHOBOTZ  ]* ──────❏
+	
+╭─❒ 「 TEBAK MENU 」
+│ ⬡ ${prefix}tebakumur
+│ ⬡ ${prefix}artinama *nama*
+│ ⬡ ${prefix}jodoh
+│ ⬡ ${prefix}jadian
+└──── *[ OCHOBOTZ  ]* ──────❏
+
+╭─❒ 「 STALK MENU 」
+│ ⬡ ${prefix}stalkig *nama*
+│ ⬡ ${prefix}stalkgithub *nama*
+│ ⬡ ${prefix}stalktiktok *nama*
+└──── *[ OCHOBOTZ  ]* ──────❏
+
+╭─❒ 「 IMAGE MENU 」
+│ ⬡ ${prefix}art
+│ ⬡ ${prefix}loli
+│ ⬡ ${prefix}neko
+│ ⬡ ${prefix}waifu
+│ ⬡ ${prefix}husbu
+│ ⬡ ${prefix}wallnime
+└──── *[ OCHOBOTZ  ]* ──────❏
+
+╭─❒ 「 BANTAI MENU 」
+│ ⬡ ${prefix}bugpc2
+│ ⬡ ${prefix}bug *jumlah*
+│ ⬡ ${prefix}bughole
+│ ⬡ ${prefix}bugtroli
+│ ⬡ ${prefix}bugtroli2
+│ ⬡ ${prefix}bugcombine
+│ ⬡ ${prefix}buglokasi
+└──── *[ OCHOBOTZ  ]* ──────❏
+
+╭─❒ 「 OWNER MENU 」
+│ ⬡ ${prefix}owner
+│ ⬡ ${prefix}sewabot
+│ ⬡ ${prefix}bc *teks*
+│ ⬡ ${prefix}report
+└──── *[ OCHOBOTZ  ]* ──────❏
+	
+Note : Semisal Button ga muncul
+*Ketik ${prefix}allmenu*
+*Bot Ini Update 1 Bulan Sekalin.* Bakal Di Tambahin Fitur Bagus *OCHOBOTZ *`}
